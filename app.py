bd9755d1f1b9078e790156a724499a417cae2974
Ubuntu <ubuntu@ip-172-31-88-29.ec2.internal>
2019-10-22 04:18:10 +0000

    setup for flask_uploads completed.

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
 import pymongo
+from flask_uploads import UploadSet, IMAGES, configure_uploads
 
 app = Flask(__name__)
 
+TOP_LEVEL_DIR = os.path.abspath(os.curdir) #1
+upload_dir = '/static/uploads/img/' #2
+app.config["UPLOADS_DEFAULT_DEST"] = TOP_LEVEL_DIR + upload_dir #3
+app.config["UPLOADED_IMAGES_DEST"] = TOP_LEVEL_DIR + upload_dir #4
+app.config["UPLOADED_IMAGES_URL"] = upload_dir #5
+
 @app.route("/")
 def home():
-    return "test data"
+    return render_template("index.template.html")
     
 #"magic code" - - boilerplate
 if __name__ == "__main__":
